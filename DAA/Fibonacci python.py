7cfe133713ce74a39fbff3414677aa4b33e0e063
ZZA1234 <114941666+ZZA1234@users.noreply.github.com>
2024-11-07 20:36:23 +0530

    Update Fibonacci python.py

--- DAA/Fibonacci python.py
+++ DAA/Fibonacci python.py
@@ -4,7 +4,7 @@ def fibonacci_iter(n):
     steps = 1
     a = 0
     b = 1
-    for i in range(2, n):
+    for i in range(2, n+1):
         c = a + b
         a = b
         b = c
@@ -12,10 +12,8 @@ def fibonacci_iter(n):
     return c, steps
 
 def fibonacci_recur(n):
-    if n ==1:
-        return 0,1
-    elif n==2:
-        return 1,1
+    if n <=1:
+        return n,1
     fib1, steps1 = fibonacci_recur(n-1)
     fib2, steps2 = fibonacci_recur(n-2)
     return fib1 + fib2, steps1 + steps2 + 1
