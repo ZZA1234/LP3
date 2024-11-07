30243b6f9edd9c8a5f06c95e912cbf6a0cb62f13
ZZA1234 <114941666+ZZA1234@users.noreply.github.com>
2024-11-07 20:45:59 +0530

    Create Fibonacci cpp.cpp

---
+++ DAA/Fibonacci cpp.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <utility>
+
+using namespace std;
+
+pair<int, int> fibonacci_iter(int n)
+{
+    if (n <= 1)
+    {
+        return {n, 1};
+    }
+    
+    int steps = 1;
+    int a = 0, b = 1, c = 0;
+    
+    for (int i = 2; i <= n; ++i)
+    {
+        c = a + b;
+        a = b;
+        b = c;
+        steps++;
+    }
+    return {c, steps};
+}
+
+pair<int, int> fibonacci_recur(int n)
+{
+    if(n <= 1)
+    {
+        return {n, 1};
+    }
+    
+    auto [fib1, steps1] = fibonacci_recur(n - 1); 
+    auto [fib2, steps2] = fibonacci_recur(n - 2);
+    
+    int fibonacci_number = fib1 + fib2;
+    int steps = steps1 + steps2 + 1;
+    
+    return {fibonacci_number, steps};
+}
+
+int main()
+{
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    
+    auto [iter_fib, iter_steps] = fibonacci_iter(n);
+    cout << "Iterative Fibonacci number: " << iter_fib << endl;
+    cout << "Iterative steps: " << iter_steps << endl;
+    
+    auto [recur_fib, recur_steps] = fibonacci_recur(n);
+    cout << "Recursive Fibonacci number: " << recur_fib << endl;
+    cout << "Recursive steps: " << recur_steps << endl;
+    
+    return 0;
+}
